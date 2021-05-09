6ebc4485efd439238c255c7bf61e4c333a0742f2
lgomez-technisys <lgomez@technisys.com>
2021-05-09 12:33:38 -0300

    Se agrega update y delete de productos y se reestructura archivo de constantes

---
+++ constants.py
@@ -0,0 +1,13 @@
+RESPONSE_CODE_OK=0
+RESPONSE_CODE_ERROR_PARAMS_REQUIRED=11
+RESPONSE_CODE_ERROR_BAD_REQUEST=400 # Podríamos definir otro codigo, por ahora usamos el mismo código de HTTP
+RESPONSE_CODE_ERROR_NOT_CONTENT=204 # Podríamos definir otro codigo, por ahora usamos el mismo código de HTTP
+RESPONSE_CODE_ERROR_PRODUCT_ALREADY_EXISTS=12
+RESPONSE_CODE_ERROR_PRODUCT_NOT_EXISTS=13
+
+RESPONSE_MESSAGE_OK='Transacción realizada con éxito'
+RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED='No se ingresaron todos los parámetros requeridos'
+RESPONSE_MESSAGE_ERROR_BAD_REQUEST='Request mal formado'
+RESPONSE_MESSAGE_ERROR_NOT_FOUND='No se encontró información con los filtros de búsqueda ingresados'
+RESPONSE_MESSAGE_ERROR_PRODUCT_ALREADY_EXISTS='Ya existe un producto con ese nombre'
+RESPONSE_MESSAGE_ERROR_PRODUCT_NOT_EXISTS='No existe el producto'
--- controller/productController.py
+++ controller/productController.py
@@ -2,10 +2,9 @@ from flask import Blueprint
 from flask import request, Response, json
 from model.product import Product, save
 from model.entityResponse import EntityResponse
-from model.constants import RESPONSE_CODE_OK, RESPONSE_MESSAGE_OK, RESPONSE_CODE_ERROR_PARAMS_REQUIRED, RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED
-from model.constants import RESPONSE_CODE_ERROR_BAD_REQUEST, RESPONSE_MESSAGE_ERROR_BAD_REQUEST, RESPONSE_MESSAGE_ERROR_NOT_FOUND, RESPONSE_CODE_ERROR_NOT_CONTENT
+from sqlalchemy import update, delete
+import constants
 from config import session
-# from model.products_to_dict import ProductsToDict
 
 productController = Blueprint('productController', __name__)
 
@@ -18,9 +17,14 @@ def createProduct():
         price = request.get_json().get('price')
 
         if name == None or description == None or price == None:
-            errorResponse = EntityResponse(RESPONSE_CODE_ERROR_PARAMS_REQUIRED, RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
+            errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PARAMS_REQUIRED, constants.RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
             return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
 
+        count = session.query(Product).filter_by(name=name).count()
+        if  count > 1:
+            errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PRODUCT_ALREADY_EXISTS, constants.RESPONSE_MESSAGE_ERROR_PRODUCT_ALREADY_EXISTS, False)
+            return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
+        
         product = Product()
         
         product.name = name
@@ -29,35 +33,87 @@ def createProduct():
 
         save(product)
 
-        response = EntityResponse(RESPONSE_CODE_OK, RESPONSE_MESSAGE_OK, True)
+        response = EntityResponse(constants.RESPONSE_CODE_OK, constants.RESPONSE_MESSAGE_OK, True)
         return Response(json.dumps(response.__dict__),status=201)
     
-    errorResponse = EntityResponse(RESPONSE_CODE_ERROR_BAD_REQUEST, RESPONSE_MESSAGE_ERROR_BAD_REQUEST, False)
+    errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_BAD_REQUEST, constants.RESPONSE_MESSAGE_ERROR_BAD_REQUEST, False)
     return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
 
 
 @productController.route('/products/getProductByName/<string:name>')
 def getProductByName(name):
     if name == None:
-        errorResponse = EntityResponse(RESPONSE_CODE_ERROR_PARAMS_REQUIRED, RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
+        errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PARAMS_REQUIRED, constants.RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
         return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
     
     try:
         product = session.query(Product).filter_by(name=name).first()
     except Exception as e:
         print(e)
-        notFoundResponse = EntityResponse(RESPONSE_CODE_ERROR_NOT_CONTENT, RESPONSE_MESSAGE_ERROR_NOT_FOUND, False)
+        notFoundResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_NOT_CONTENT, constants.RESPONSE_MESSAGE_ERROR_NOT_FOUND, False)
         return Response(json.dumps(notFoundResponse.__dict__), status=404, mimetype='application/json')
     
     if product != None:
         print(product.__dict__)
-        response = EntityResponse(RESPONSE_CODE_OK, RESPONSE_MESSAGE_OK, True)
+        response = EntityResponse(constants.RESPONSE_CODE_OK, constants.RESPONSE_MESSAGE_OK, True)
         return Response(json.dumps(product.products_to_dict()),status=200)
     
-    notFoundResponse = EntityResponse(RESPONSE_CODE_ERROR_NOT_CONTENT, RESPONSE_MESSAGE_ERROR_NOT_FOUND, False)
+    notFoundResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_NOT_CONTENT, constants.RESPONSE_MESSAGE_ERROR_NOT_FOUND, False)
     return Response(json.dumps(notFoundResponse.__dict__), status=404, mimetype='application/json')
 
 
-@productController.route('/products/deleteProduct', methods=['DELETE'])
-def deleteProduct():
-    return 'ok'
+#Este método actualiza todos los atributos de la tabla product.
+#En caso de no recibir alguno de los atributos retorna un error.
+@productController.route('/products/update/<string:name>', methods=['PUT'])
+def updateProduct(name):
+
+    if request.get_json() and name != None:
+        description = request.get_json().get('description')
+        price = request.get_json().get('price')
+
+        count = session.query(Product).filter_by(name=name).count()
+        if  count == 0:
+            errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PRODUCT_NOT_EXISTS, constants.RESPONSE_MESSAGE_ERROR_PRODUCT_NOT_EXISTS, False)
+            return Response(json.dumps(errorResponse.__dict__), status=404, mimetype='application/json')
+
+        if description == None or price == None:
+            errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PARAMS_REQUIRED, constants.RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
+            return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
+
+        stmt = (
+            update(Product).
+            where(Product.name==name).
+            values(description=description, price=price)
+        )
+        print(stmt)
+        session.execute(stmt)
+        session.commit()
+
+        response = EntityResponse(constants.RESPONSE_CODE_OK, constants.RESPONSE_MESSAGE_OK, True)
+        return Response(json.dumps(response.__dict__),status=201)
+    
+    errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_BAD_REQUEST, constants.RESPONSE_MESSAGE_ERROR_BAD_REQUEST, False)
+    return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
+
+
+@productController.route('/products/delete/<string:name>', methods=['DELETE'])
+def deleteProduct(name):
+    if name == None:
+        errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PARAMS_REQUIRED, constants.RESPONSE_MESSAGE_ERROR_PARAMS_REQUIRED, False)
+        return Response(json.dumps(errorResponse.__dict__), status=400, mimetype='application/json')
+    
+    count = session.query(Product).filter_by(name=name).count()
+    if  count == 0:
+        errorResponse = EntityResponse(constants.RESPONSE_CODE_ERROR_PRODUCT_NOT_EXISTS, constants.RESPONSE_MESSAGE_ERROR_PRODUCT_NOT_EXISTS, False)
+        return Response(json.dumps(errorResponse.__dict__), status=404, mimetype='application/json')
+    
+    stmt = (
+        delete(Product).
+        where(Product.name==name)
+    )
+    print(stmt)
+    session.execute(stmt)
+    session.commit()
+    
+    response = EntityResponse(constants.RESPONSE_CODE_OK, constants.RESPONSE_MESSAGE_OK, True)
+    return Response(json.dumps(response.__dict__),status=200)
