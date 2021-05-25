798274f8242885d2449a0771d6ae8f50e4554846
lgomez-technisys <lgomez@technisys.com>
2021-05-24 21:28:07 -0300

    Version inicial del frontend

---
+++ webCart/src/components/CardProducts.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" >
+      <q-img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+      <q-card-section>
+        <div class="text-h6">{{title}}</div>
+        <div class="text-subtitle2">{{subtitle}}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        {{ description }}
+      </q-card-section>
+    </q-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      default: 'Subtitulo'
+    },
+    description: {
+      type: String,
+      default: 'description'
+    },
+    image: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
---
+++ webCart/src/pages/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <CardProducts
+        v-for="data in productsData"
+        :key="data.title"
+        v-bind="data"/>
+  </div>
+</template>
+
+<script>
+import CardProducts from 'components/CardProducts.vue'
+
+const productsData = [
+  {
+    title: 'Producto1',
+    subtitle: 'Subtitulo1',
+    description: 'Descripcion 1',
+    image: 'https://cdn.quasar.dev/img/mountains.jpg'
+  },
+  {
+    title: 'Producto2',
+    subtitle: 'Subtitulo2',
+    description: 'Descripcion 2',
+    image: 'https://cdn.quasar.dev/img/mountains.jpg'
+  },
+  {
+    title: 'Producto3',
+    subtitle: 'Subtitulo3',
+    description: 'Descripcion 3',
+    image: 'https://cdn.quasar.dev/img/mountains.jpg'
+  }
+]
+
+export default {
+  name: 'PageIndex',
+  components: { CardProducts },
+  data () {
+    return {
+      productsData
+    }
+  }
+}
+</script>
